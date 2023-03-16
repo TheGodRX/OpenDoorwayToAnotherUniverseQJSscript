0db99f580422ed39ee5a971e8471acb350c5e138
C0N5013 <30527348+codercoins@users.noreply.github.com>
2023-03-16 12:50:46 -0400

    Update DONOTUSETHISEVER!!Q.js

--- DONOTUSETHISEVER!!Q.js
+++ DONOTUSETHISEVER!!Q.js
@@ -1,61 +1,139 @@
-// Import necessary libraries for quantum computing
-const { Circuit, Simulator } = require('quantum-js');
-
-// Define the current universe parameters based on user input
-const currentDimensions = parseInt(prompt("Enter the number of dimensions for the current universe: "));
-const currentLaws = prompt("Enter the physical laws for the current universe: ");
-const currentParticles = prompt("Enter the fundamental particles for the current universe: ");
-
-// Define the desired universe parameters based on user input
-const dimensions = parseInt(prompt("Enter the number of dimensions for the new universe: "));
-const laws = prompt("Enter the physical laws for the new universe: ");
-const particles = prompt("Enter the fundamental particles for the new universe: ");
-
-// Create a quantum circuit with the necessary qubits and gates
-const circuit = Circuit.create(dimensions);
-
-// Apply quantum gates to create the desired quantum state
-// (we will assume that a quantum algorithm has been developed to generate the desired state)
-
-// Create a superposition of the current universe and the desired universe
-circuit.hadamard(0);
-
-// Measure the qubit to collapse the superposition and open a doorway to the new universe
-circuit.measureAll();
-
-// Define a function to turn the key and open the door to the new universe
-function turnTheKey() {
-  const simulator = new Simulator();
-  const results = simulator.run(circuit, 1);
-
-  if (results[0] === '1'.repeat(dimensions)) {
-    console.log("The key has successfully turned and the doorway to the new universe has been opened!");
-    console.log("Welcome to the new universe with the following parameters: ");
-    console.log("Dimensions: ", dimensions);
-    console.log("Physical laws: ", laws);
-    console.log("Fundamental particles: ", particles);
-    
-    // Ask the user if they want to swap to the new universe
-    const swap = prompt("Do you want to swap to the new universe? (y/n)");
-    if (swap === "y") {
-      console.log("You have successfully swapped to the new universe.");
-      console.log("The previous universe had the following parameters: ");
-      console.log("Dimensions: ", currentDimensions);
-      console.log("Physical laws: ", currentLaws);
-      console.log("Fundamental particles: ", currentParticles);
-    } else {
-      console.log("You chose not to swap to the new universe.");
+// Define Circuit class
+class Circuit {
+    constructor(numQubits) {
+      this.numQubits = numQubits;
+      this.state = Array.from({length: 2**numQubits}, () => 0);
+      this.state[0] = 1;
     }
-    
-  } else {
-    console.log("The key failed to turn. Please try again.");
+  
+    hadamard(target) {
+      const root2 = 1 / Math.sqrt(2);
+      const newState = new Array(this.state.length);
+      for (let i = 0; i < this.state.length; i++) {
+        const bit = (i >> target) & 1;
+        const sign = bit === 0 ? 1 : -1;
+        const amplitude = this.state[i] * root2 * sign;
+        const nextState1 = i ^ (1 << target);
+        const nextState2 = nextState1 ^ (1 << target);
+        newState[nextState1] = (newState[nextState1] || 0) + amplitude;
+        newState[nextState2] = (newState[nextState2] || 0) + amplitude;
+      }
+      this.state = newState;
+    }
+  
+    measureAll() {
+      let totalProbability = 0;
+      for (let i = 0; i < this.state.length; i++) {
+        totalProbability += this.state[i] ** 2;
+      }
+      const probabilities = new Array(this.state.length);
+      let cumulativeProbability = 0;
+      for (let i = 0; i < this.state.length; i++) {
+        probabilities[i] = this.state[i] ** 2 / totalProbability;
+        cumulativeProbability += probabilities[i];
+        probabilities[i] = cumulativeProbability;
+      }
+      const random = Math.random();
+      let collapsedState = -1;
+      for (let i = 0; i < this.state.length; i++) {
+        if (random <= probabilities[i]) {
+          collapsedState = i;
+          break;
+        }
+      }
+      for (let i = 0; i < this.state.length; i++) {
+        this.state[i] = 0;
+      }
+      this.state[collapsedState] = 1;
+    }
   }
-}
-
-// Ask the user if they want to turn the key to open the door to the newly created universe
-const turnKey = prompt("Do you want to turn the key to open the doorway to the new universe? (y/n)");
-if (turnKey === "y") {
-  turnTheKey();
-} else {
-  console.log("The doorway to the new universe remains closed.");
-}
+  
+  // Add error handling for askQuestion function
+  const readline = require('readline');
+  
+  function askQuestion(question) {
+    return new Promise((resolve, reject) => {
+      const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout
+      });
+      rl.question(question, (answer) => {
+        // Check for empty input
+        if (answer.trim() === '') {
+          reject(new Error("Input cannot be empty."));
+        } else {
+          rl.close();
+          resolve(answer);
+        }
+      });
+    });
+  }
+  
+  async function main() {
+    try {
+      // Define the current universe parameters based on user input
+      const currentDimensions = parseInt(await askQuestion("Enter the number of dimensions for the current universe: "));
+      const currentLaws = await askQuestion("Enter the physical laws for the current universe: ");
+      const currentParticles = await askQuestion("Enter the fundamental particles for the current universe: ");
+  
+      // Define the desired universe parameters based on user input
+      const dimensions = parseInt(await askQuestion(`Enter the number of dimensions for the new universe (current universe has ${currentDimensions} dimensions): `));
+      const laws = await askQuestion(`Enter the physical laws for the new universe (current laws: ${currentLaws}): `);
+      const particles = await askQuestion(`Enter the fundamental particles for the new universe (current particles: ${currentParticles}): `);
+  
+      // Create a quantum circuit with the necessary qubits and gates
+      const circuit = new Circuit(dimensions);
+  
+      // Apply quantum gates to create the desired quantum state
+      for (let i = 0; i < dimensions; i++) {
+        circuit.hadamard(i);
+      }
+  
+      // Measure the qubits to collapse the superposition and open a doorway to the new universe
+      circuit.measureAll();
+  
+      // Define a function to turn the key and open the door to the new universe
+      function turnTheKey() {
+        const start = 2**(dimensions-1);
+        const end = 2**dimensions-1;
+        const newState = circuit.state.slice(start, end+1);
+        const sum = newState.reduce((acc, val) => acc + val, 0);
+        if (sum === 1) {
+          console.log("The key has successfully turned and the doorway to the new universe has been opened!");
+          console.log("Welcome to the new universe with the following parameters: ");
+          console.log("Dimensions: ", dimensions);
+          console.log("Physical laws: ", laws);
+          console.log("Fundamental particles: ", particles);
+  
+          // Ask the user if they want to swap to the new universe
+          askQuestion("Do you want to swap to the new universe? (y/n)").then((swap) => {
+            if (swap === "y") {
+              console.log("You have successfully swapped to the new universe.");
+              console.log("The previous universe had the following parameters: ");
+              console.log("Dimensions: ", currentDimensions);
+              console.log("Physical laws: ", currentLaws);
+              console.log("Fundamental particles: ", currentParticles);
+            } else {
+              console.log("You chose not to swap to the new universe.");
+            }
+          });
+  
+        } else {
+          console.log("The key failed to turn. Please try again.");
+        }
+      }
+  
+      // Ask the user if they want to turn the key to open the door to the newly created universe
+      askQuestion("Do you want to turn the key to open the doorway to the new universe? (y/n)").then((turnKey) => {
+        if (turnKey === "y") {
+          turnTheKey();
+        } else {
+          console.log("The doorway to the new universe remains closed.");
+        }
+      });
+    } catch (error) {
+      console.log(error.message);
+    }
+  }
+  
+  main();
